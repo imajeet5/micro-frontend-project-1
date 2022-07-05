777ab4a715b1750b1b9304f09a41ca46e549cdd1
Ajeet Singh <imajeet5@gmail.com>
2022-07-05 16:05:14 +0530

    Finished feature #1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,8 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there!!!</h1>
+      <h1>Hi there!!</h1>
+      <p>Added Production workflow</p>
       <hr />
       <MarketingApp />
     </div>
