d4503af1dd90e65eced37ee748afae57321371c8
Ajeet Singh <imajeet5@gmail.com>
2022-07-05 16:27:31 +0530

    fix branch pushing not working

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there!!</h1>
+      <h1>Hi there</h1>
       <p>Added Production workflow</p>
       <hr />
       <MarketingApp />
